ce84b54ad071a04853cc59f273b914f704401b30
shaurya singh <shauryasinghcsai@gmail.com>
2026-06-11 23:07:34 +0530

    prime number range finder in Java

---
+++ day 3 uploaded/primenumberinrange.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class primenumberinrange {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter the starting number");
+        int start=sc.nextInt();
+
+        System.out.println("enter the ending number");
+        int end =sc.nextInt();
+        System.out.println("prime number between " +start+ " and " +end+ "are");
+        for (int i=start ;i<=end; i++){
+            if( isPrime(i)){
+                System.out.println(i+" ");
+
+            }
+        }
+        sc.close();
+    }
+public static boolean isPrime(int num){
+    if(num<=1){
+        return false;
+    }
+    for(int i=2;i<=Math.sqrt(num);i++){
+        if(num%i==0){
+            return false;
+        }
+      }
+     return true;
+    }
+}
+    
