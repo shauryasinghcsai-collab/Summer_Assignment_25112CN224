eb69afd0a072056d04a9d61c96ee71a2c03a4718
shaurya singh <shauryasinghcsai@gmail.com>
2026-06-18 08:52:56 +0530

    Add repeated character pattern program

---
+++ day9 uploaded/repeatednumbertriangle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class repeatedcharectorpatter {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+        char ch='A';
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch + " "); ;
+            }
+            ch++;
+            System.out.println();
+        }
+        scanner.close();
+    }
+    
+}
