ce08252318b93457b7612bc403b763ac5cba1c92
shaurya singh <shauryasinghcsai@gmail.com>
2026-06-19 14:10:50 +0530

    Add pyramid of numbers program in Java

---
+++ day 10 uploaded/pyramidofanumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class pyramidofnumbers {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of rows for the pyramid:");
+        int rows =sc.nextInt();
+        for(int i=1;i<=rows;i++){
+            for(int j=1;j<=rows-i;j++){
+                System.out.print("  ");
+                }
+                int j;
+                for(j=1;j<=i;j++){
+                    System.out.print(j+" ");
+                }
+                for(j=i-1;j>=1;j--){
+                    System.out.print(j+" ");
+                }
+                System.out.println();
+            }
+        
+        sc.close();
+    }
+}
